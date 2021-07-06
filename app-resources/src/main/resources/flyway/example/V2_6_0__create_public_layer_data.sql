3166fed9638bdf9ac7eb1b27c74c7ec643755e11
Urban Planning Technology <63308105+UPTechMX@users.noreply.github.com>
2021-07-06 17:05:59 -0500

    Update V2_6_0__create_public_layer_data.sql

--- app-resources/src/main/resources/flyway/example/V2_6_0__create_public_layer_data.sql
+++ app-resources/src/main/resources/flyway/example/V2_6_0__create_public_layer_data.sql
@@ -1,8 +1,8 @@
 CREATE SEQUENCE public.public_layer_data_id_seq
     INCREMENT 1
-    START 1
+    START 1073741822
     MINVALUE 1
-    MAXVALUE 9223372036854775807
+    MAXVALUE 2147483647
     CACHE 1;
 
 ALTER SEQUENCE public.public_layer_data_id_seq
